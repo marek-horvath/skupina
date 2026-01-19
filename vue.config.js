5ada8a5d5257496f76cbd2eb020cee201ff0b2ac
Marek <marek.horvath@tuke.sk>
2026-01-19 17:34:08 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/portfolio/' // Pridaj názov svojho repozitára
+  publicPath: '/skupina/' // Pridaj názov svojho repozitára
 })
